fc1918152a813df7a6aa48f9e0a735e209dddab2
PurpleRaccoon <jkemery@capital.edu>
2018-05-04 19:16:59 -0400

    Switched from tabs to spaces

--- MDtoHTML.py
+++ MDtoHTML.py
@@ -2,74 +2,75 @@
 import sys
 
 def main(argv):
-    if len(argv) > 1:
-        mdToHTML(argv[1])
-    else:
-        mdToHTML("Testing/CanvasHelp.md")
+	if len(argv) > 1:
+		mdToHTML(argv[1])
+	else:
+		mdToHTML("Testing/CanvasHelp.md")
+		
 
 def mdToHTML(mdFileName):
 
-    # open markdown file
-    mdFile = open(mdFileName, 'r')
+	# open markdown file
+	mdFile = open(mdFileName, 'r')
 
-    # TODO: generate HTML file
-    TITLE = ["#", "##"]
-    LIST = ["*"]
+	# TODO: generate HTML file
+	TITLE = ["#", "##"]
+	LIST = ["*"]
 
-    htmlText = []
-    bootstrapText = []
+	htmlText = []
+	bootstrapText = []
 
-    introduction = True
-    tocSectionTitles = []
+	introduction = True
+	tocSectionTitles = []
 
-    sectionLines = []
-    listLines = []
+	sectionLines = []
+	listLines = []
 
-    for line in mdFile:
-        words = line.split()
-        # check if line is blank
-        if len(words) > 1:
-            # check if line is a section title
-            if words[0] in TITLE:
-                if not introduction:
-                    sectionFrom(sectionLines)
-                    tocSectionTitles.append(words[1:])
-                sectionLines = [words[1:]]
+	for line in mdFile:
+		words = line.split()
+		# check if line is blank
+		if len(words) > 1:
+			# check if line is a section title
+			if words[0] in TITLE:
+				if not introduction:
+					sectionFrom(sectionLines)
+					tocSectionTitles.append(words[1:])
+				sectionLines = [words[1:]]
 
-            else:
-                # add line to section
-                sectionLines.append(line)
+			else:
+				# add line to section
+				sectionLines.append(line)
 
-    # create file for HTML output
-    # remove '.md' extension and add '.html'
-    htmlFileName = mdFileName[:-3] + ".html"
-    writeHTMLFile("\n".join(htmlText), htmlFileName)
+	# create file for HTML output
+	# remove '.md' extension and add '.html'
+	htmlFileName = mdFileName[:-3] + ".html"
+	writeHTMLFile("\n".join(htmlText), htmlFileName)
 
-    bootstrapFileName = mdFileName[:-3] + "Bootstrap.html"
-    writeHTMLFile("\n".join(bootstrapText), bootstrapFileName)
+	bootstrapFileName = mdFileName[:-3] + "Bootstrap.html"
+	writeHTMLFile("\n".join(bootstrapText), bootstrapFileName)
 
-    # close file
-    mdFile.close()
+	# close file
+	mdFile.close()
 
 def writeHTMLFile(text, fileName):
-    # open HTML file for writing
-    htmlFile = open(fileName, 'w')
+	# open HTML file for writing
+	htmlFile = open(fileName, 'w')
 
-    # write out file
-    print(text, file=htmlFile)
+	# write out file
+	print(text, file=htmlFile)
 
-    # close file
-    htmlFile.close()
+	# close file
+	htmlFile.close()
 
 def sectionFrom(lines):
-    # TODO: section head
-    for line in lines:
-        # TODO: process each line
-        pass
-    # TODO: section tail
+	# TODO: section head
+	for line in lines:
+		# TODO: process each line
+		pass
+	# TODO: section tail
 
 
 
 if __name__ == '__main__':
-    main(sys.argv)
-    
+	main(sys.argv)
+	
